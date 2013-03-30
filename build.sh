3050cd45e937b6424aaefaa601f820378a9d0828
Jan Lehnardt <jan@apache.org>
2013-03-30 23:26:33 +0100

    1.3.0

--- build.sh
+++ build.sh
@@ -4,12 +4,12 @@
 # Run build-couchdb to do a fully automated build of the recent CouchDB 
 
 if [ ! -d build-couchdb ]; then
-  git clone git://github.com/cloudnode/build-couchdb
+  git clone git://github.com/iriscouch/build-couchdb
 fi
 cd build-couchdb
   git submodule init
   git submodule update
-  rake git="https://git-wip-us.apache.org/repos/asf/couchdb.git tags/1.2.1" install="$HOME/build"
+  rake git="https://git-wip-us.apache.org/repos/asf/couchdb.git 48879873f5316c6ef1dcc6de91ea2f5b2721efb0" install="/Users/jan/build"
 cd ..
 
 if [ ! -d couchdbx-app ]; then
