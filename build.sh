ec47ba98788a91ad4a70a11cfc086ada5fa99d95
Jan Lehnardt <jan@apache.org>
2017-10-21 10:22:55 +0200

    feat: dynamic dep detection

--- build.sh
+++ build.sh
@@ -1,19 +1,22 @@
-#!/bin/sh -x
+#!/bin/sh -xe
 
 # clean &  create builddir
 BUILDDIR=/tmp/couchdbx-core
 rm -rf $BUILDDIR couchdb-mac-app
 mkdir -p $BUILDDIR
 
+DESTDIR=./build
+rm -rf $DESTDIR
+mkdir -p $DESTDIR
+
 # prepare build deps
-brew update
-brew install erlang spidermonkey icu4c md5sha1sum
-brew link -f icu4c
+# brew update
+# brew install erlang spidermonkey icu4c md5sha1sum
+# brew link -f icu4c
 
 # get latest couchdb release:
 rm -rf apache-couchdb-*
 wget https://dist.apache.org/repos/dist/release/couchdb/source/2.1.0/apache-couchdb-2.1.0.tar.gz
-mv apache-couchdb-2.1.0-RC1.tar.gz apache-couchdb-2.1.0.tar.gz
 tar xzf apache-couchdb-*
 
 # build couchdb
@@ -34,11 +37,13 @@ COUCHDB_VERSION=`ls apache-couchdb-* | head -n 1 | grep -Eo '(\d+\.\d+\.\d+)' | 
 #
 # cp -r $SOURCES $BUILDDIR
 
+ICUDATA_VERSION=`ls /usr/local/opt/icu4c/lib/libicuuc.??.?.dylib | grep -o '\d\d\.\d'`
+ICUUCI18N_VERSION=`ls /usr/local/opt/icu4c/lib/libicuuc.??.dylib | grep -o '\d\d'`
+
 # copy icu & ssl && nspr libs to safety
-cp /usr/local/opt/icu4c/lib/libicuuc.58.dylib \
-   /usr/local/opt/icu4c/lib/libicudata.58.dylib \
-   /usr/local/opt/icu4c/lib/libicudata.58.2.dylib \
-   /usr/local/opt/icu4c/lib/libicui18n.58.dylib \
+cp /usr/local/opt/icu4c/lib/libicuuc.$ICUUCI18N_VERSION.dylib \
+   /usr/local/opt/icu4c/lib/libicudata.$ICUDATA_VERSION.dylib \
+   /usr/local/opt/icu4c/lib/libicui18n.$ICUUCI18N_VERSION.dylib \
    /usr/local/opt/openssl/lib/libcrypto.1.0.0.dylib \
    /usr/local/opt/nspr/lib/libplds4.dylib \
    /usr/local/opt/nspr/lib/libplc4.dylib \
@@ -62,23 +67,27 @@ adjust_name() {
     TARGET=$3
     chmod +w $TARGET
     install_name_tool -change $FROM $TO $TARGET
+    if [ $? ~ne 0 ]; then
+      echo "FAIL $TARGET"
+      exit 1
+    fi
     chmod -w $TARGET
 }
 
 # adjust couch_icu_driver linking
-adjust_name /usr/local/opt/icu4c/lib/libicudata.58.2.dylib lib/libicudata.58.2.dylib lib/couch-*/priv/couch_icu_driver.so
-adjust_name /usr/local/opt/icu4c/lib/libicuuc.58.dylib lib/libicuuc.58.dylib lib/couch-*/priv/couch_icu_driver.so
-adjust_name /usr/local/opt/icu4c/lib/libicui18n.58.dylib lib/libicui18n.58.dylib lib/couch-*/priv/couch_icu_driver.so
-adjust_name @loader_path/libicudata.58.dylib @loader_path/libicudata.58.2.dylib lib/libicui18n.58.dylib
-adjust_name @loader_path/libicudata.58.dylib @loader_path/libicudata.58.2.dylib lib/libicuuc.58.dylib
+adjust_name /usr/local/opt/icu4c/lib/libicudata.$ICUDATA_VERSION.dylib lib/libicudata.$ICUDATA_VERSION.dylib lib/couch-*/priv/couch_icu_driver.so
+adjust_name /usr/local/opt/icu4c/lib/libicuuc.$ICUUCI18N_VERSION.dylib lib/libicuuc.$ICUUCI18N_VERSION.dylib lib/couch-*/priv/couch_icu_driver.so
+adjust_name /usr/local/opt/icu4c/lib/libicui18n.$ICUUCI18N_VERSION.dylib lib/libicui18n.$ICUUCI18N_VERSION.dylib lib/couch-*/priv/couch_icu_driver.so
+adjust_name @loader_path/libicudata.$ICUUCI18N_VERSION.dylib lib/libicudata.$ICUDATA_VERSION.dylib lib/libicui18n.$ICUUCI18N_VERSION.dylib
+adjust_name @loader_path/libicudata.$ICUUCI18N_VERSION.dylib lib/libicudata.$ICUDATA_VERSION.dylib lib/libicuuc.$ICUUCI18N_VERSION.dylib
 
 # adjust couch_ejson_compare linking
-adjust_name /usr/local/opt/icu4c/lib/libicudata.58.2.dylib lib/libicudata.58.2.dylib lib/couch-*/priv/couch_ejson_compare.so
-adjust_name /usr/local/opt/icu4c/lib/libicuuc.58.dylib lib/libicuuc.58.dylib lib/couch-*/priv/couch_ejson_compare.so
-adjust_name /usr/local/opt/icu4c/lib/libicui18n.58.dylib lib/libicui18n.58.dylib lib/couch-*/priv/couch_ejson_compare.so
+adjust_name /usr/local/opt/icu4c/lib/libicudata.$ICUDATA_VERSION.dylib lib/libicudata.$ICUDATA_VERSION.dylib lib/couch-*/priv/couch_ejson_compare.so
+adjust_name /usr/local/opt/icu4c/lib/libicuuc.$ICUUCI18N_VERSION.dylib lib/libicuuc.$ICUUCI18N_VERSION.dylib lib/couch-*/priv/couch_ejson_compare.so
+adjust_name /usr/local/opt/icu4c/lib/libicui18n.$ICUUCI18N_VERSION.dylib lib/libicui18n.$ICUUCI18N_VERSION.dylib lib/couch-*/priv/couch_ejson_compare.so
 
 # adjust crypto.so
-adjust_name /usr/local/opt/openssl/lib/libcrypto.1.0.0.dylib lib/libcrypto.1.0.0.dylib lib/crypto-3.4.2/priv/lib/crypto.so
+adjust_name /usr/local/opt/openssl/lib/libcrypto.1.0.0.dylib lib/libcrypto.1.0.0.dylib lib/crypto-*/priv/lib/crypto.so
 
 # adjust couchjs
 adjust_name /usr/local/opt/spidermonkey/lib/libmozjs185.1.0.dylib lib/libmozjs185.1.0.dylib bin/couchjs
@@ -175,15 +184,16 @@ cd -
 git clone git://github.com/janl/couchdb-mac-app.git couchdb-mac-app
 
 cd couchdb-mac-app
-  perl -pi.bak -e "s/\<string\>VERSION\<\/string\>/<string>$COUCHDB_VERSION<\/string>/" Couchbase\ Server/Apache\ CouchDB-Info.plist
+  perl -pi.bak -e "s/\<string\>VERSION\<\/string\>/<string>$COUCHDB_VERSION<\/string>/" CouchDB\ Server/Apache\ CouchDB-Info.plist
   xcodebuild clean
   xcodebuild
 cd ..
 
-DESTDIR=$HOME/build/janl/build-couchdb-mac/build
-rm -rf $DESTDIR
-mkdir -p $DESTDIR
+cp couchdb-mac-app/build/Release/Apache-*.zip $DESTDIR
 
-cp couchdb-mac-app/build/Release/Apache-*.zip* $DESTDIR
+cd $DESTDIR
+ZIPFILE=`ls Apache-*.zip`
+shasum -a 256 Apache-*.zip > $ZIPFILE.sha256
+shasum -a 512 Apache-*.zip > $ZIPFILE.sha512
 
-cd $HOME/build/janl/build-couchdb-mac
+echo "now run gpg --armor --detach-sig $DESTDIR/$ZIPFILE > $DESTDIR/$ZIPFILE.asc"
